48f4e106674d60200ea21db996a7d22b4fdc009d
ondralukes <mail@ondralukes.cz>
2019-11-21 09:05:04 +0100

    Added overwrite dialog
    Fixed #12

---
+++ src/xml/xmlGenerator/OverwriteDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xmlGenerator
+{
+    public partial class OverwriteDialog : Form
+    {
+        public string filename;
+        public bool overwrite;
+        public bool forAll;
+        public bool inputOk = false;
+        public OverwriteDialog(string _filename)
+        {
+            filename = _filename;
+            InitializeComponent();
+            mainLabel.Text = $"{filename} already exists\nOverwrite?";
+        }
+
+        private void YesBtn_Click(object sender, EventArgs e)
+        {
+            overwrite = true;
+            forAll = false;
+            inputOk = true; 
+            Close();
+        }
+
+        private void YesForAllBtn_Click(object sender, EventArgs e)
+        {
+            overwrite = true;
+            forAll = true;
+            inputOk = true;
+            Close();
+        }
+
+        private void NoBtn_Click(object sender, EventArgs e)
+        {
+            overwrite = false;
+            forAll = false;
+            inputOk = true;
+            Close();
+        }
+
+        private void NoForAllBtn_Click(object sender, EventArgs e)
+        {
+            overwrite = false;
+            forAll = true;
+            inputOk = true;
+            Close();
+        }
+    }
+}
